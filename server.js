91befbe94ae9aac19e3e61155c5f8bdcea3e01de
ureshii-ww <wontshow@roflmail.com>
2021-01-23 15:58:10 +0700

    Made the base of API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const port = 8000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,15 +19,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:date", (req, res) => {
+  let date = new Date(req.params.date);
 
+  if (date.toString() === 'Invalid Date') {
+    res.json({error: date.toString()})
+  }
+  else {
+    res.json({
+      utc: date.getTime(),
+      date: date.toUTCString()
+    });
+  }
+
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
